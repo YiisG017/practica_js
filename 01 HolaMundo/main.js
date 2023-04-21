427987129f92fe92bbfe8fedcb3b4b28180246fe
Johan <gitzzgg@gmail.com>
2023-04-21 10:54:58 -0600

    hola cara de bola

---
+++ 01 HolaMundo/main.js
@@ -0,0 +1,10 @@
+const hola=()=>{
+    let nombre= prompt("¿Cual es tu nombre?"+ "🤍")
+    alert("Hola "+nombre+"🤍") 
+  }
+  const suma=()=>{
+    let numero1 = parseInt (prompt("Numero 1:"))
+    let numero2 = parseInt (prompt("Numero 2:"))
+    let suma = numero1+numero2;
+    alert ("suma:"+suma)
+  }
---
+++ 04 Imagen/main.js
@@ -0,0 +1,12 @@
+const uno=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="pp.jpg";
+  }
+  const dos=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="helloK.jpg";
+  }
+  const tres=()=>{
+    let img=document.querySelector("#imagen");
+    img.src="jh.jpg";
+  }
